4de196eb153e7dbc497f3527c9391e7ba5b7ab27
zybeasy <zybeasy@163.com>
2019-08-06 15:58:24 +0800

    fix

--- sword/utils/List.h
+++ sword/utils/List.h
@@ -12,13 +12,13 @@ struct ListNode
     ListNode* m_pNext;
 };
 
-__declspec( dllexport ) ListNode* CreateListNode(int value);
-__declspec( dllexport ) void ConnectListNodes(ListNode* pCurrent, ListNode* pNext);
-__declspec( dllexport ) void PrintListNode(ListNode* pNode);
-__declspec( dllexport ) void PrintList(ListNode* pHead);
-__declspec( dllexport ) void DestroyList(ListNode* pHead);
-__declspec( dllexport ) void AddToTail(ListNode** pHead, int value);
-__declspec( dllexport ) void RemoveNode(ListNode** pHead, int value);
+ListNode* CreateListNode(int value);
+void ConnectListNodes(ListNode* pCurrent, ListNode* pNext);
+void PrintListNode(ListNode* pNode);
+void PrintList(ListNode* pHead);
+void DestroyList(ListNode* pHead);
+void AddToTail(ListNode** pHead, int value);
+void RemoveNode(ListNode** pHead, int value);
 
 
 #endif //CPPDEMO_LIST_H
